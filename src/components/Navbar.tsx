1979f9c1a733006a819524a7099f693ccf49b5ca
Rodrigo costa <re.costa@campus.fct.unl.pt>
2024-11-12 23:58:31 +0000

    idk menu button e first html

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,31 +1,78 @@
-import './Navbar.css';
-import { useState } from 'react';
-function Navbar() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+import React, { useState } from "react";
+import "./Navbar.css";
+import "./Menu.css";
+
+const Navbar: React.FC = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen((prevState) => !prevState); // Toggle menu open/close
+  };
 
   return (
-    <>
-      <nav className='navbar'>
-        <div className='navbar-container'>
-          <div  className='navbar-logo'>
-            <img className='navbar-icon' src='logo.png'/>
-            TRACK AND TASTE
-          </div>
-          <div className='menu-icon'>
-            <i className='fas fa-bars'/>
-          </div>
-          <div className='nav-menu'>
+    <nav className="navbar">
+      <div className="navbar-container">
+        <div className="navbar-logo">
+          <img className="navbar-icon" src="logo.png" alt="Site Logo" />
+          TRACK AND TASTE
+        </div>
+
+        <div className="nav-right">
           {isLoggedIn ? (
-              <img className='fas fa-user-circle navbar-icon' src='logo.png' />  // Shows the user icon if logged in
-            ) : (
-              <button onClick={() => setIsLoggedIn(true)}>Log In</button> // Shows the Log In button if not logged in
-            )}
-            <img className='fas fa-user-circle navbar-icon' src='logo.png'></img>
+            <button
+              onClick={() => setIsLoggedIn(false)}
+              className="login-button"
+            >
+              Log Out
+            </button>
+          ) : (
+            <button
+              onClick={() => setIsLoggedIn(true)}
+              className="login-button"
+            >
+              Log In
+            </button>
+          )}
+
+          {/* Hamburger Menu Icon */}
+          <div className="menu-icon" onClick={toggleMenu}>
+            <div className="hamburger">
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
           </div>
         </div>
-      </nav>
-    </>
+      </div>
+
+      {/* Conditional Rendering of Dropdown Menu */}
+      {isMenuOpen && (
+        <div className="dropdown-menu">
+          <ul>
+            <li>
+              <a href="/recipe-book.html">My Recipe Book</a>
+            </li>
+            <li>
+              <a href="#profile">Add Recipe</a>
+            </li>
+            <li>
+              <a href="#settings">Calorie Track</a>
+            </li>
+            <li>
+              <a href="#n">Nutritionist</a>
+            </li>
+            <li>
+              <a href="#p">Profile</a>
+            </li>
+            <li>
+              <a href="#s">Settings</a>
+            </li>
+          </ul>
+        </div>
+      )}
+    </nav>
   );
-}
+};
 
 export default Navbar;
