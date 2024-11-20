405d97d14428e53e5525de5675e4ed8c66364324
Martim Latas <m.latas@campus.fct.unl.pt>
2024-11-20 20:35:48 +0000

    Top Picks Commit
    Creation and addition of the Top Picks for Today component to the main page

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,12 +1,16 @@
 import Navbar from "../components/Navbar";
 import MainScreen from "../components/MainScreen";
 import SearchBar from "../components/SearchBar";
+import TopPicks from "../components/TopPicks";
 
 export function Home() {
   return (
     <>
       <div>
         <SearchBar />
+      </div>
+      <div>
+        <TopPicks />
       </div>
       <div>
         <Navbar />
